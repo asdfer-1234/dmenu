5cbf62bb6485bdef0e5f48c6cac93c1863f43296
asdfer-1234 <sh__lv@naver.com>
2023-03-29 09:53:02 +0900

    fix

--- config.h
+++ config.h
@@ -2,9 +2,10 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static unsigned int barmargin = 4;
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"Liberation Mono:size=19"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
