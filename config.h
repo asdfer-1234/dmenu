e57877c19676779fa656071ad1463248ff51502d
asdfer-1234 <sh__lv@naver.com>
2023-04-05 10:05:49 +0900

    aaaaaaaa

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static unsigned int barmargin = 4;
+static unsigned int barmargin = 8;
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"JetBrainsMono:size=12"
